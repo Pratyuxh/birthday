d52c66a848db99dea051585d37fdd958f83732c2
Pratyush Sharma <pratyushsharma@Pratyushs-MacBook-Air.local>
2023-06-06 11:39:57 +0530

    React project

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        name: "Pratyush Sharma",
+        age: 24,
+    },
+    {
+        id: 2,
+        name: "Ria Sharma",
+        age: 21,
+    },
+    {
+        id: 3,
+        name: "Gaurav Sharma",
+        age: 23,
+    },
+    {
+        id: 4,
+        name: "Chirag Sharma",
+        age: 27,
+    },
+];
